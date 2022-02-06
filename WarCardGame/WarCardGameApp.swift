316e6b651c2cde62f158dfeb5fb48d5fe4742d16
RaviKiranReddyS <ravikiranreddy.soorasani@gmail.com>
2022-02-06 14:18:08 -0500

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Ravi Kiran Reddy on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
